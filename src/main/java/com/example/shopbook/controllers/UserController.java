6abb36aeaf95d40d72f75b1f814f73a6899b1632
EsercitoHaki <esercitohaki2003@gmail.com>
2024-06-01 07:19:13 +0700

    Đăng ký và đăng nhập tài khoản

---
+++ src/main/java/com/example/shopbook/controllers/UserController.java
@@ -0,0 +1,46 @@
+package com.example.shopbook.controllers;
+
+import com.example.shopbook.dtos.*;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+    @PostMapping("/register")
+    public ResponseEntity<?> createUser(
+            @Valid @RequestBody UserDTO userDTO,
+            BindingResult result){
+        try {
+            if (result.hasErrors()){
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            if (!userDTO.getPassword().equals(userDTO.getRetypePassword())){
+                return ResponseEntity.badRequest().body("Mật khẩu không trùng nhau");
+            }
+            return ResponseEntity.ok("Đăng ký thành công");
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(
+            @Valid @RequestBody UserLoginDTO userDTO){
+        //Kiểm tra thông tin đăng nhập và sinh ra token
+        //Trả về token trong response
+        return ResponseEntity.ok("một số token");
+    }
+}
---
+++ src/main/java/com/example/shopbook/dtos/UserLoginDTO.java
@@ -0,0 +1,20 @@
+package com.example.shopbook.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserLoginDTO {
+    @NotBlank(message = "Số điện thoại là bắt buộc")
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    @NotBlank(message = "Mật khẩu không được để trống")
+    private String password;
+}
